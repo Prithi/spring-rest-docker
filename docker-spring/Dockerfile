07dc2ef63902e6da20a469308198822effe58445
Prithi <43105765+Prithi@users.noreply.github.com>
2021-05-26 19:42:49 +0530

    Initial

---
+++ docker-spring/Dockerfile
@@ -0,0 +1,4 @@
+FROM openjdk:8
+ADD target/currenttime.jar currenttime.jar
+EXPOSE 8073
+ENTRYPOINT ["java", "-jar", "currentime.jar"]
---
+++ docker-spring/src/main/java/com/assignment/docker/dockerspring/resources/TimeResource.java
@@ -0,0 +1,19 @@
+package com.assignment.docker.dockerspring.resources;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("GET /time")
+public class TimeResource {
+
+    @GetMapping
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+
+        public void values(){
+        for (DateTimeFormat.ISO c : DateTimeFormat.ISO.values())
+            System.out.println(c);
+    }
+}
